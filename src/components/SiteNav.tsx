69f035fb37a7da23dc7d6ac4034e0addbe8b02f7
developer-ananya <178642881+developer-ananya@users.noreply.github.com>
2026-05-11 19:31:42 +0530

    logo fix4

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -18,17 +18,17 @@ export function SiteNav() {
     <nav id="nav" className={navScrolled ? "solid compact" : undefined}>
       <Link href="/" className="nav-logo-wrap" aria-label="The Vansh Group">
         <span
-          className="nav-mark-slot relative flex size-[92px] shrink-0 items-center justify-center overflow-hidden max-lg:size-[70px] max-[480px]:size-[64px]"
+          className="nav-mark-slot relative flex size-[100px] shrink-0 items-center justify-center overflow-hidden max-lg:size-[76px] max-[480px]:size-[68px]"
           aria-hidden="true"
         >
-          <span className="relative flex size-[135px] shrink-0 origin-center scale-[calc(92/135)] items-center justify-center max-lg:scale-[calc(70/135)] max-[480px]:scale-[calc(64/135)]">
+          <span className="relative flex size-[135px] shrink-0 origin-center scale-[calc(100/135)] items-center justify-center max-lg:scale-[calc(76/135)] max-[480px]:scale-[calc(68/135)]">
             <Image
               src="/vansh-logo-mark.png"
               alt=""
               width={500}
               height={500}
-              sizes="(max-width: 480px) 64px, (max-width: 1024px) 70px, 92px"
-              className="nav-mark-img relative h-auto w-[54%] shrink-0 object-contain"
+              sizes="(max-width: 480px) 68px, (max-width: 1024px) 76px, 100px"
+              className="nav-mark-img relative h-auto w-[56%] shrink-0 object-contain"
               priority
             />
           </span>
